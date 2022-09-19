cc428668173dde12fdaab11b757e44bf1eb5cb13
AKSizov <37084670+AKSizov@users.noreply.github.com>
2022-09-19 14:22:43 -0400

    made it look marginally better

--- LAB2/roses.py
+++ LAB2/roses.py
@@ -23,13 +23,11 @@ rose2 = (5, 1, 2)
 rose3 = (5, 7, 2)
 rose4 = (1, 1, 10)
 rose5 = (1, 1, 1)
-lens = [0, 0, 0, 0, 0]
-lens[0] = sum(rose1) + 3
-lens[1] = sum(rose2) + 3
-lens[2] = sum(rose3) + 3
-lens[3] = sum(rose4) + 3
-lens[4] = sum(rose5) + 3
-
+lens = [sum(rose1) + 3,
+        sum(rose2) + 3,
+        sum(rose3) + 3,
+        sum(rose4) + 3,
+        sum(rose5) + 3]
 longest = max(lens)
 
 draw_line(rose1, longest)
