45160d35bb64aed6151177a2dc5cf8c50db6fb68
AKSizov <37084670+AKSizov@users.noreply.github.com>
2022-09-19 14:13:48 -0400

    added lab2

---
+++ LAB2/roses.py
@@ -0,0 +1,39 @@
+# roses.py
+# lab 2.1
+
+#      @-->---->---
+#       @----->->--
+# @----->------->--
+#   @->->----------
+#            @->->-
+
+# what a nice rose.
+
+def draw_line(vals, maxlen):
+    line1 = "-" * vals[0]
+    line2 = "-" * vals[1]
+    line3 = "-" * vals[2]
+    full = "@%s>%s>%s" % (line1, line2, line3)
+    print(full.rjust(maxlen))
+
+
+# sure, loops would be prettier. but they're still not allowed.
+rose1 = (2, 4, 3)
+rose2 = (5, 1, 2)
+rose3 = (5, 7, 2)
+rose4 = (1, 1, 10)
+rose5 = (1, 1, 1)
+lens = [0, 0, 0, 0, 0]
+lens[0] = sum(rose1) + 3
+lens[1] = sum(rose2) + 3
+lens[2] = sum(rose3) + 3
+lens[3] = sum(rose4) + 3
+lens[4] = sum(rose5) + 3
+
+longest = max(lens)
+
+draw_line(rose1, longest)
+draw_line(rose2, longest)
+draw_line(rose3, longest)
+draw_line(rose4, longest)
+draw_line(rose5, longest)
